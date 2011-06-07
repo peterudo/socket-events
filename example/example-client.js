db39aebbbbee09bad6a70120a92c126a768510cd
Peter Rudolfsen <peter@silencethefoe.com>
2011-06-07 23:11:16 +0200

    Cleanup

--- example/example-client.js
+++ example/example-client.js
@@ -3,15 +3,15 @@ var sock = require('../lib/json-socket');
 var client = sock.connect(8124, 'localhost');
 
 client.on('connect', function () {
-	console.log("I just connected to the server...");
+    console.log("I just connected to the server...");
 });
 
 client.on('event_on_client', function (data) {
-	console.log('received JSON from server:');
-	console.log(data);
+    console.log('received JSON from server:');
+    console.log(data);
 });
 
 client.trigger('event_on_server', {
-	data: 'sent to the server',
-	from: 'the client'
+    data: 'sent to the server',
+    from: 'the client'
 });
--- example/example-server.js
+++ example/example-server.js
@@ -1,17 +1,17 @@
 var sock = require('../lib/json-socket');
 
 var server = sock.create(function (client) {
-	console.log('A new client connected...');
+    console.log('A new client connected...');
 
-	client.on('event_on_server', function (data) {
-		console.log('received JSON from a client:');
-		console.log(data);
-	});
+    client.on('event_on_server', function (data) {
+        console.log('received JSON from a client:');
+        console.log(data);
+    });
 
-	client.trigger('event_on_client', {
-		data: 'sent from server',
-		to: 'client'
-	});
+    client.trigger('event_on_client', {
+        data: 'sent from server',
+        to: 'client'
+    });
 });
 
 server.listen(8124, 'localhost');
