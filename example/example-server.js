462746a7dcf493e9ec41db4d194770f5d5a4685a
Peter Rudolfsen <peter@silencethefoe.com>
2011-06-07 23:08:44 +0200

    Added support for custom events both ways

--- example/example-server.js
+++ example/example-server.js
@@ -3,42 +3,15 @@ var sock = require('../lib/json-socket');
 var server = sock.create(function (client) {
 	console.log('A new client connected...');
 
-	client.on('data', function (data) {
-		console.log(data.toString());
+	client.on('event_on_server', function (data) {
+		console.log('received JSON from a client:');
+		console.log(data);
 	});
 
-//	client.trigger('init', { who: 'areyou?' });
+	client.trigger('event_on_client', {
+		data: 'sent from server',
+		to: 'client'
+	});
 });
 
 server.listen(8124, 'localhost');
-
-
-/*
-var net = require('net');
-
-var server = net.createServer(function (socket) {
-	console.log(socket);
-	socket.write("heihei");
-
-	socket.on('data', function (data) {
-		console.log(data.toString());
-	});
-
-	socket.on('connect', function (client) {
-		console.log('someone connected');
-	});
-
-	socket.on('heihei', function () {
-		console.log("HEIHEI");
-	});
-
-	setInterval(function () {
-		socket.write(JSON.stringify({
-			job: "job1"
-		}));
-	}, 1000);
-
-});
-
-server.listen(8124, 'localhost');
-*/
